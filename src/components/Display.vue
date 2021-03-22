fefe271845602d2b03c5cb3c7c64d17a8761c990
Savio Nascimento <savionascimentodev@gmail.com>
2021-03-22 11:58:45 -0400

    Finished Project✔️

---
+++ src/components/Display.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="display">
+    {{ value }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['value']
+}
+</script>
+
+<style>
+  .display {
+    grid-column: span 4 ;
+    background-color: #0004;
+
+    border-radius: 0 ;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    padding: 20px;
+    font-size: 2.1em;
+    overflow: hidden;
+
+    border-bottom: var(--border-button);
+  }
+</style>
