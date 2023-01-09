f4be615c9c02e461ba72e4057f20f14f266f4f98
Savio Nascimento <savionascimentodev@gmail.com>
2023-01-09 19:43:51 -0400

    ✨ added new api coin project

---
+++ api-coin/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container grid-lg my-2 py-2">
+    <table class="table table-striped table-hover">
+      <thead>
+        <tr>
+          <th>name</th>
+          <th>genre</th>
+          <th>release date</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="active">
+          <td>The Shawshank Redemption</td>
+          <td>Crime, Drama</td>
+          <td>14 October 1994</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
